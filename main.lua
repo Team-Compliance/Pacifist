ad59dcc354c18e34b82c90598bb8d3ba00752b37
anchikai <hentaisuperstar86@gmail.com>
2022-08-29 14:51:18 -0400

    Skipping Item Rooms and Shops grant golden chests

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 PacifistMod = RegisterMod("Pacifist", 1)
 local mod = PacifistMod
+local game = Game()
 
 CollectibleType.COLLECTIBLE_PACIFIST = Isaac.GetItemIdByName("Pacifist")
 
@@ -33,18 +34,23 @@ if Encyclopedia then
 end
 
 local PickupsToSpawn = 0
+local ChestsToSpawn = 0
 
 function mod:PacifistEffect(player)
 	local sprite = player:GetSprite()
 	if player:HasCollectible(CollectibleType.COLLECTIBLE_PACIFIST) and sprite:IsPlaying("Trapdoor") then
 		if PickupsToSpawn == 0 then
-			local level = Game():GetLevel()
+			local level = game:GetLevel()
 			local rooms = level:GetRooms()
 			for i = 0, rooms.Size - 1 do
 				local room = rooms:Get(i)
 				if room.Data.Type ~= RoomType.ROOM_SUPERSECRET and room.Data.Type ~= RoomType.ROOM_ULTRASECRET then -- based off of world card which doesn't reveal these
 					if not room.Clear then
 						PickupsToSpawn = PickupsToSpawn + 1
+						if room.Data.Type == RoomType.ROOM_SHOP or room.Data.Type == RoomType.ROOM_TREASURE then
+							ChestsToSpawn = ChestsToSpawn + 1
+							PickupsToSpawn = PickupsToSpawn - 1
+						end
 					end
 				end
 			end
@@ -53,21 +59,25 @@ function mod:PacifistEffect(player)
 end
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, mod.PacifistEffect)
 
-function mod:PickupsDrop() --spawn pickups every level after pickup
-	for p = 0, Game():GetNumPlayers() - 1 do
-		local player = Game():GetPlayer(p)
+function mod:PickupsDrop() -- Spawn pickups every level after pickup
+	for p = 0, game:GetNumPlayers() - 1 do
+		local player = game:GetPlayer(p)
 		if player:HasCollectible(CollectibleType.COLLECTIBLE_PACIFIST) then
 			if PickupsToSpawn > 0 then
 				SFXManager():Play(SoundEffect.SOUND_THUMBSUP, 1, 0)
 				player:AnimateHappy()
 			end
-			
+
 			for i = 1, PickupsToSpawn do
-				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_NULL, 2, Game():GetRoom():FindFreePickupSpawnPosition(player.Position, 0, true), Vector.Zero, player)
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_NULL, 2, game:GetRoom():FindFreePickupSpawnPosition(player.Position, 0, true), Vector.Zero, player)
+			end
+			-- Guaranteed Golden Chests if you skip a Treasure or Shop room
+			for j = 1, ChestsToSpawn do
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LOCKEDCHEST, ChestSubType.CHEST_CLOSED, game:GetRoom():FindFreePickupSpawnPosition(player.Position, 0, true), Vector.Zero, player)
 			end
 		end
 	end
-	
 	PickupsToSpawn = 0
+	ChestsToSpawn = 0
 end
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.PickupsDrop)
